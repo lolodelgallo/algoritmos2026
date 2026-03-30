02c94486790b896b12806082bac1da86cc1c5cb3
Lorenzo Etchegaray <extremepvp003@gmail.com>
2026-03-30 19:17:58 -0300

    Actualizacion trabajoPractico

---
+++ trabajopractico1.py
@@ -0,0 +1,6 @@
+print ('hello world')
+print ('Calculador de potencias')
+base = int(input('Ingrese la base: '))
+exponente = int(input('Ingrese el exponente: '))
+resultado = base ** exponente
+print (f'El resultado de {base} elevado a {exponente} es: {resultado}')
